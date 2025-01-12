a4226e44c53e23c815cf5709ae762b9549096521
Nodir8 <Nodirbek8464>
2025-01-13 05:30:03 +0900

    feat: publish othernavbar

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,4 +1,62 @@
-import { Container } from "@mui/material";
+import { Container, Box, Stack, Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function OtherNavbar() {
-  return <Container>OtherNavbar</Container>;
+  const authMember = null;
+  return (
+    <div className="other-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box className="home-line">
+            <NavLink to="/" activeClassName="underline">
+              <img className="brand-logo" src="/icons/burak.svg" />
+            </NavLink>
+          </Box>
+          <Stack className="links">
+            <Box className="home-line">
+              <NavLink to="/">Home</NavLink>{" "}
+            </Box>
+            <Box className="home-line">
+              <NavLink to="/products" activeClassName="underline">
+                Products
+              </NavLink>{" "}
+            </Box>
+            {authMember ? (
+              <Box className="home-line">
+                <NavLink to="/orders" activeClassName="underline">
+                  Orders
+                </NavLink>{" "}
+              </Box>
+            ) : null}
+            {authMember ? (
+              <Box className="home-line">
+                <NavLink to="/members-page" activeClassName="underline">
+                  My page
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className="home-line">
+              <NavLink to="/help" activeClassName="underline">
+                Help
+              </NavLink>
+            </Box>
+            {/** basket */}
+            {!authMember ? (
+              <Box>
+                <Button variant="contained" className="login-button">
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src="/icons/default-user.svg"
+                aria-haspopup={"true"}
+              />
+            )}
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
