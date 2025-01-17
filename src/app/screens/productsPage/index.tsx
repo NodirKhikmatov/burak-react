9b0521bb9edc4b0d09c7eae496362550bc242958
Nodir8 <Nodirbek8464>
2025-01-17 22:46:56 +0900

    feat: create page routing system

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,4 +1,22 @@
 import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import ChosenProduct from "./ChosenProduct";
+import Products from "./Products";
+
 export default function ProductsPage() {
-  return <Container>ProductPage</Container>;
+  const products = useRouteMatch();
+  console.log("Products", products);
+
+  return (
+    <div className={"products-age"}>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
