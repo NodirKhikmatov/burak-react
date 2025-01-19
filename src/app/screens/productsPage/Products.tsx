e2fd213684a86f9d0483f251aa3e2fa6ca789525
Nodir8 <Nodirbek8464>
2025-01-20 04:12:07 +0900

    publish product section component

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -1,3 +1,190 @@
+import React from "react";
+import { Box, Button, Container, Stack } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import Badge from "@mui/material/Badge";
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const products = [
+  { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab.webp" },
+  { productName: "Lavash", imagePath: "/img/lavash.webp" },
+  { productName: "Lavash", imagePath: "/img/lavash.webp" },
+  { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab.webp" },
+];
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <div className={"products"}>
+      <Container>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Stack className={"avatar-big-box"}>
+            <Stack className="top-title">
+              <Box className="top-text">Burak Restaurant</Box>
+              <Box className="single-search">
+                <input
+                  className="single-search-input"
+                  placeholder="Type here"
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className="single-button-search"
+                >
+                  search
+                  <SearchIcon />
+                </Button>
+              </Box>
+            </Stack>
+          </Stack>
+
+          <Stack className={"dishes-filter-section"}>
+            <Stack className={"dishes-filter-box"}>
+              <Button
+                variant={"contained"}
+                color={"primary"}
+                className={"order"}
+              >
+                New
+              </Button>
+              <Button
+                variant={"contained"}
+                color={"secondary"}
+                className={"order"}
+              >
+                Price
+              </Button>
+              <Button
+                variant={"contained"}
+                color={"secondary"}
+                className={"order"}
+              >
+                Views{" "}
+              </Button>
+            </Stack>
+          </Stack>
+
+          <Stack className={"list-category-section"}>
+            <Stack className={"product-category"}>
+              <div className={"category-main"}>
+                <Button variant={"contained"} color={"secondary"}>
+                  Other
+                </Button>
+                <Button variant={"contained"} color={"secondary"}>
+                  Dessert
+                </Button>
+                <Button variant={"contained"} color={"secondary"}>
+                  Drink
+                </Button>
+                <Button variant={"contained"} color={"secondary"}>
+                  Salad
+                </Button>
+                <Button variant={"contained"} color={"secondary"}>
+                  Dish
+                </Button>
+              </div>
+            </Stack>
+
+            <Stack className={"product-wrapper"}>
+              {products.length !== 0 ? (
+                products.map((product, index) => {
+                  return (
+                    <Stack key={index} className={"product-card"}>
+                      <Stack
+                        className={"product-img"}
+                        sx={{ backgroundImage: `url(${product.imagePath})` }}
+                      >
+                        <div className={"product-sale"}> Normal size </div>
+                        <Button className={"shop-btn"}>
+                          <img
+                            src={"/icons/shopping-cart.svg"}
+                            style={{ display: "flex" }}
+                          />
+                        </Button>
+                        <Button className={"view-btn"} sx={{ right: "36px" }}>
+                          <Badge badgeContent={20} color={"secondary"}>
+                            <RemoveRedEyeIcon sx={{ color: "gray" }} />
+                          </Badge>
+                        </Button>
+                      </Stack>
+                      <Box className={"product-desc"}>
+                        <span className={"product-title"}>
+                          {product.productName}
+                        </span>
+                        <div className={"product-desc"}>
+                          <MonetizationOnIcon />
+                          15
+                        </div>
+                      </Box>
+                    </Stack>
+                  );
+                })
+              ) : (
+                <Box className={"no-data"}>Products are not available</Box>
+              )}
+            </Stack>
+          </Stack>
+
+          <Stack className={"pagination-section"}>
+            <Pagination
+              count={3}
+              page={1}
+              renderItem={(item) => (
+                <PaginationItem
+                  components={{
+                    previous: ArrowBackIcon,
+                    next: ArrowForwardIcon,
+                  }}
+                  {...item}
+                  color={"secondary"}
+                />
+              )}
+            />
+          </Stack>
+        </Stack>
+      </Container>
+
+      <div className={"brands-logo"}>
+        <Container>
+          <Box className="brand-text">Our Family Brands </Box>
+          <Stack className="brand-cards">
+            <Box className="brand-card">
+              <img src="/img/seafood.webp" alt="" />
+            </Box>
+            <Box className="brand-card">
+              <img src="/img/doner.webp" alt="" />
+            </Box>
+            <Box className="brand-card">
+              <img src="/img/sweets.webp" alt="" />
+            </Box>
+            <Box className="brand-card">
+              <img src="/img/gurme.webp" alt="" />
+            </Box>
+          </Stack>
+        </Container>
+      </div>
+
+      <div className={"address"}>
+        <Container>
+          <Stack className={"address-area"}>
+            <Box className={"title"}>Our address </Box>
+            <iframe
+              style={{ marginTop: "60px" }}
+              src="https://www.google.com/maps?q=Burak+restaurand+istanbul&amp;t&amp;z=13&amp;ie=UTF8&amp;iwloc&amp;output=embed"
+              height="500px"
+              width="1320px"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Stack>
+        </Container>
+      </div>
+    </div>
+  );
 }
--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -2,13 +2,14 @@ import { Container } from "@mui/material";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import ChosenProduct from "./ChosenProduct";
 import Products from "./Products";
+import "../../../css/products.css";
 
 export default function ProductsPage() {
   const products = useRouteMatch();
   console.log("Products", products);
 
   return (
-    <div className={"products-age"}>
+    <div className={"products-page"}>
       <Switch>
         <Route path={`${products.path}/:productId`}>
           <ChosenProduct />
