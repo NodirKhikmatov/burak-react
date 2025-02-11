0dcd603a4b209a2c0e47f0800fc0734ccec8bcfc
Nodir8 <Nodirbek8464>
2025-02-11 16:20:13 +0900

    feat:retrieve frew menu and develop new dishew

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setPopularDishes, setNewDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enum/product.enum";
@@ -18,13 +18,12 @@ import { ProductCollection } from "../../../lib/enum/product.enum";
 
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
-
-
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
-  
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
+
   //selector:store => Data
 
   useEffect(() => {
@@ -41,6 +40,19 @@ export default function HomePage() {
       .then((data) => {
         console.log("data passed here", data);
         setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+        // productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        console.log("data passed here", data);
+        setNewDishes(data);
       })
       .catch((err) => console.log(err));
   }, []);
