a8e04a344fd02dd10f0c0b93767e66a8e16bb972
Nodir8 <Nodirbek8464>
2025-02-13 16:32:30 +0900

    feat: develop chosenProduct

--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -12,11 +12,10 @@ class ProductService {
   public async getProducts(input: ProductInquiry): Promise<Product[]> {
     try {
       let url = `${this.path}/product/all?order=${input.order}&page=${input.page}&limit=${input.limit}`;
-     
+
       if (input.productCollection)
         url += `&productCollection=${input.productCollection}`;
       if (input.search) url += `&search=${input.search}`;
-
 
       const result = await axios.get(url);
       console.log("getProducts", result);
@@ -24,6 +23,19 @@ class ProductService {
       return result.data;
     } catch (err) {
       console.log("error: getProducts", err);
+      throw err;
+    }
+  }
+
+  public async getProduct(productId: string): Promise<Product> {
+    try {
+      const url = `${this.path}/product/${productId}`;
+      const result = await axios.get(url, { withCredentials: true });
+      console.log("keldi2", result);
+
+      return result.data;
+    } catch (err) {
+      console.log("err get product", err);
       throw err;
     }
   }
